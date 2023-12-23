c2000c6897201a96bf7eb7e5d0959ea4c61982c9
Zhongheng-Cheng <zc2737@columbia.edu>
2023-12-22 19:06:03 -0500

    added bugs relating to file reading

--- data/graphql_test.py
+++ data/graphql_test.py
@@ -13,8 +13,9 @@ class Query(graphene.ObjectType):
     users = graphene.List(Userss)
 
     def resolve_users(self, info):
-        with open("users.json", "r") as in_file:
-            users_data = json.load(in_file)
+        # with open("users.json", "r") as in_file:
+        #     users_data = json.load(in_file)
+        users_data = [{"user_id": "aa1234", "user_name": "aatest", "password": "password", "email": "aa1234@columbia.edu", "profile_picture": "1", "role": "Student"}, {"user_id": "bb1234", "user_name": "bbtest", "password": "password", "email": "bb1234@columbia.edu", "profile_picture": "1", "role": "Admin"}, {"user_id": "cc1234", "user_name": "cctest", "password": "password", "email": "cc1234@columbia.edu", "profile_picture": "1", "role": "Student"}, {"user_id": "dd1234", "user_name": "ddtest", "password": "password", "email": "dd1234@columbia.edu", "profile_picture": "1", "role": "Student"}, {"user_id": "ee1234", "user_name": "eetest", "password": "password", "email": "ee1234@columbia.edu", "profile_picture": "1", "role": "IT Faculty"}, {"user_id": "ff1234", "user_name": "fftest", "password": "password", "email": "ff1234@columbia.edu", "profile_picture": "1", "role": "Student"}]
         users = [Userss(**user) for user in users_data]
         # return Userss(user_id= "aa1234", user_name= "aatest", password= "password", email= "aa1234@columbia.edu", profile_picture= "1", role= "Student")
         return users
